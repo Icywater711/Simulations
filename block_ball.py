7b28d8fc73352edc1ba59d21057fde53e13f4a84
Icywater711 <halsaleh2008@gmail.com>
2025-11-02 11:13:21 +0000

    Work in progress / latest changes

--- block_ball.py
+++ block_ball.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation 
 
 # constants
-M = 10.0 # mass of the block
-m = 7.0  # mass of the ball
+M = 1000.0 # mass of the block
+m = 1.0  # mass of the ball
 dt = 0.01  # time step
-steps = 1000  # number of time steps
+steps = 5000  # number of time steps
+x = 2.5  # size of the block and ball
 
 # intial conditions
 r =np.array([-3.0, 0.0]) # position of the block and ball respectively
@@ -16,29 +17,40 @@ trajectories = []
 
 
 for _ in range(steps):
-    d = r[1]-r[0]  # distance between block and ball
-    if v[0] > v[1] and d <=2:  # collision detection
-        v = N @ v  # update velocities after collision
 
-    if r[1] >= 20:  # ball hits the wall
+    d = r[1]-r[0]  # distance between block and ball
+
+    if d <= x:  # collision detected
+        v = N @ abs(v) 
+
+    if d <= x and m*v[1] > M*v[0]: 
+        v = N @ v
+    
+    if r[1] >= 19:  # ball hits the wall
         v[1] = -v[1]  # reverse ball velocity
 
     r += v*dt  # update positions
-    if r[1]<= r[0]+2:  # prevent overlap
-        r[1] = r[0]+2
+    if r[1]<= r[0]+x:  # prevent overlap
+        r[1] = r[0]+x
+
+    if r[1]>=19:  # prevent ball from going past wall
+        r[1] = 19
     trajectories.append(r.copy())
 
 trajectories = np.array(trajectories)
 
 #animation setup
-fig, ax = plt.subplots(figsize=(10,10), dpi=100)
+fig, ax = plt.subplots(figsize=(8,8), dpi=300)
 ax.set_xlim(-20,20)
 ax.set_ylim(-20,20)
 ax.set_aspect('equal')
+ax.axis('off')
+ax.set_facecolor("black")
+fig.patch.set_facecolor("black")
 
 block, = ax.plot([], [], 's', markersize=50, color='blue')
 ball, = ax.plot([], [], 'o', markersize=8, color='red')
-
+line = ax.plot([19.5,19.5], [-20,20], color='white', linewidth=2)
 def update(frame):
     block.set_data([trajectories[frame, 0]], [0])
     ball.set_data([trajectories[frame, 1]], [0])
@@ -51,5 +63,5 @@ def init():
 
 ani = animation.FuncAnimation(fig, update, frames=steps, init_func=init, blit=True, interval=10)
 
-ani.save("Block_Ball.mp4", fps=30, extra_args=['-vcodec', 'libx264'])
+ani.save("Block_Ball.mp4", fps=120, extra_args=['-vcodec', 'libx264'])
 
